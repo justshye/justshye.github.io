28134895c5e753517d996c5fdb5c5dd92e34515c
shye <113232835+justshye@users.noreply.github.com>
2023-05-23 16:44:56 -0700

    flips back over if a match isn't found

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const setup = () => {
     let firstCard = undefined
     let secondCard = undefined
     $(".card").on(("click"), function () {
-      $(this).toggleClass("flip");
+        $(this).toggleClass("flip");
 
-      if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
-    else {
-      secondCard = $(this).find(".front_face")[0]
-      console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      )
-        console.log("match")
-      else
-        console.log("no match")
-    }
-    
+        if (!firstCard)
+            firstCard = $(this).find(".front_face")[0]
+        else {
+            secondCard = $(this).find(".front_face")[0]
+            console.log(firstCard, secondCard);
+            if (
+                firstCard.src
+                ==
+                secondCard.src
+            )
+                console.log("match")
+            else {
+                console.log("no match")
+                $(`#${firstCard.id}`).parent().toggleClass("flip")
+                $(`#${secondCard.id}`).parent().toggleClass("flip")
+            }
+        }
+
     });
-  }
-  
-  $(document).ready(setup)
+}
+
+$(document).ready(setup)
