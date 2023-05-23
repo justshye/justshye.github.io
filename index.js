a8c72b50a8afe7899affa8c990743ca959a030dd
shye <113232835+justshye@users.noreply.github.com>
2023-05-23 14:16:38 -0700

    checks if two clicked cards are the same

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-
 const setup = () => {
+    let firstCard = undefined
+    let secondCard = undefined
     $(".card").on(("click"), function () {
       $(this).toggleClass("flip");
+
+      if (!firstCard)
+      firstCard = $(this).find(".front_face")[0]
+    else {
+      secondCard = $(this).find(".front_face")[0]
+      console.log(firstCard, secondCard);
+      if (
+        firstCard.src
+        ==
+        secondCard.src
+      )
+        console.log("match")
+      else
+        console.log("no match")
+    }
+    
     });
   }
   
