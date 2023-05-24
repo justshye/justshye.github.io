b9af0a3bd6d03b441c89ede1dc8d536a7218b84b
shye <113232835+justshye@users.noreply.github.com>
2023-05-23 22:22:09 -0700

    adds header with game information

--- index.js
+++ index.js
@@ -1,80 +1,142 @@
 const setup = () => {
     let firstCard = undefined;
     let secondCard = undefined;
-    let canFlip = true; // Add a flag to control flipping of cards
-    let pairsFound = 0; // Counter for the number of pairs found
-    const totalPairs = $(".card").length / 2; // Calculate the total number of pairs
-  
+    let canFlip = false;
+    let clickCount = 0;
+    let pairsMatched = 0;
+    let pairsLeft = 0;
+    let timerInterval = null;
+    const totalPairs = $(".card").length / 2;
+
+    $("#start-button").on("click", startGame);
+    $("#reset-button").on("click", resetGame);
+
     $(".card").on(("click"), function () {
-      // Check if flipping is allowed
-      if (!canFlip) return;
-  
-      $(this).toggleClass("flip");
-  
-      if (!firstCard)
-        firstCard = $(this).find(".front_face")[0];
-      else {
-        secondCard = $(this).find(".front_face")[0];
-        console.log(firstCard, secondCard);
-        if (firstCard.src == secondCard.src) {
-          console.log("match");
-          $(`#${firstCard.id}`).parent().off("click");
-          $(`#${secondCard.id}`).parent().off("click");
-          resetCards(); // Reset the selected cards
-          pairsFound++;
-          checkVictory(); // Check if all pairs have been found
-        } else {
-          console.log("no match");
-          canFlip = false; // Disable flipping temporarily
-          setTimeout(() => {
-            $(`#${firstCard.id}`).parent().toggleClass("flip");
-            $(`#${secondCard.id}`).parent().toggleClass("flip");
-            resetCards(); // Reset the selected cards
-          }, 1000);
+        if (!canFlip) return;
+
+        $(this).toggleClass("flip");
+        clickCount++;
+
+        if (!firstCard)
+            firstCard = $(this).find(".front_face")[0];
+        else {
+            secondCard = $(this).find(".front_face")[0];
+            console.log(firstCard, secondCard);
+            if (firstCard.src == secondCard.src) {
+                console.log("match");
+                $(`#${firstCard.id}`).parent().off("click");
+                $(`#${secondCard.id}`).parent().off("click");
+                resetCards();
+                pairsMatched++;
+                pairsLeft--;
+                updateGameInfo();
+                checkVictory();
+            } else {
+                console.log("no match");
+                canFlip = false;
+                setTimeout(() => {
+                    $(`#${firstCard.id}`).parent().toggleClass("flip");
+                    $(`#${secondCard.id}`).parent().toggleClass("flip");
+                    resetCards();
+                }, 1000);
+            }
         }
+
+        updateGameInfo();
+    });
+
+    function startGame() {
+        resetGame();
+        canFlip = true;
+        pairsLeft = totalPairs;
+        updateGameInfo();
+        startTimer(60); // 60 seconds timer
+    }
+
+    function resetGame() {
+        stopTimer();
+        resetCards();
+        clickCount = 0;
+        pairsMatched = 0;
+        pairsLeft = 0;
+        canFlip = false;
+        updateGameInfo();
+        $(".card").removeClass("flip").on("click");
+      
+        // Remove the victory message if it exists
+        $(".victory-message").remove();
       }
-    });
-  
-    // Light Theme button click event
-    $("#light-theme").on("click", function () {
-      $("body").removeClass("dark-theme").addClass("light-theme");
-      $(".card").removeClass("dark-theme").addClass("light-theme");
-      $(".theme-button").removeClass("dark-theme").addClass("light-theme");
-    });
-  
-    // Dark Theme button click event
-    $("#dark-theme").on("click", function () {
-      $("body").removeClass("light-theme").addClass("dark-theme");
-      $(".card").removeClass("light-theme").addClass("dark-theme");
-      $(".theme-button").removeClass("light-theme").addClass("dark-theme");
-    });
-  
-    // Function to reset the selected cards
+      
+
     function resetCards() {
-      firstCard = undefined;
-      secondCard = undefined;
-      canFlip = true; // Re-enable flipping
+        firstCard = undefined;
+        secondCard = undefined;
+        canFlip = true;
     }
-  
-    // Function to check if all pairs have been found
+
     function checkVictory() {
-      if (pairsFound === totalPairs) {
-        displayVictoryMessage();
-      }
+        if (pairsMatched === totalPairs) {
+            displayVictoryMessage();
+            stopTimer();
+        }
     }
-  
-    // Function to display the victory message
+
+    function updateGameInfo() {
+        $("#click-count").text(`Clicks: ${clickCount}`);
+        $("#pairs-left").text(`Pairs Left: ${pairsLeft}`);
+        $("#pairs-matched").text(`Pairs Matched: ${pairsMatched}`);
+        $("#total-pairs").text(`Total Pairs: ${totalPairs}`);
+    }
+
     function displayVictoryMessage() {
-      // Create a div element for the message
-      const victoryMessage = $("<div>")
-        .addClass("victory-message")
-        .text("Congratulations! You found all the pairs!");
-  
-      // Append the message to the body
-      $("body").append(victoryMessage);
+        const victoryMessage = $("<div>")
+            .addClass("victory-message")
+            .text("Congratulations! You found all the pairs!");
+
+        $("body").append(victoryMessage);
     }
-  };
+
+    function startTimer(duration) {
+        let timer = duration * 1000; // Convert seconds to milliseconds
+        let minutes, seconds;
+
+        timerInterval = setInterval(function () {
+            minutes = Math.floor((timer / (1000 * 60)) % 60);
+            seconds = Math.floor((timer / 1000) % 60);
+
+            minutes = minutes.toString().padStart(2, "0");
+            seconds = seconds.toString().padStart(2, "0");
+
+            $("#time-left").text(`Time Left: ${minutes}:${seconds}`);
+
+            if (timer <= 0) {
+                clearInterval(timerInterval);
+                $("#time-left").text("Time's up!");
+                canFlip = false;
+            }
+
+            timer -= 1000;
+        }, 1000);
+    }
+
+    function stopTimer() {
+        clearInterval(timerInterval);
+    }
+
+// Light Theme button click event
+$("#light-theme").on("click", function () {
+    $("body").removeClass("dark-theme").addClass("light-theme");
+    $(".card").removeClass("dark-theme").addClass("light-theme");
+    $(".theme-button").removeClass("dark-theme").addClass("light-theme");
+  });
   
-  $(document).ready(setup);
+  // Dark Theme button click event
+  $("#dark-theme").on("click", function () {
+    $("body").removeClass("light-theme").addClass("dark-theme");
+    $(".card").removeClass("light-theme").addClass("dark-theme");
+    $(".theme-button").removeClass("light-theme").addClass("dark-theme");
+  });
   
-  
+};
+
+$(document).ready(setup);
